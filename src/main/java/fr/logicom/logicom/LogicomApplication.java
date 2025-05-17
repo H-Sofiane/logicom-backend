6c155677fd802edbcc66b6657661d503c77dc953
H-Sofiane <haddad-sofiane@live.fr>
2025-05-17 10:42:27 +0200

    Initial commit

---
+++ src/main/java/fr/logicom/logicom/LogicomApplication.java
@@ -0,0 +1,13 @@
+package fr.logicom.logicom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogicomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogicomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/logicom/logicom/LogicomApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.logicom.logicom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogicomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
