3f925b663b51423aeb5551c0503cf6bab26eebf2
H-Sofiane <haddad-sofiane@live.fr>
2025-05-21 10:53:40 +0200

    adding brand attributes

--- src/main/java/fr/logicom/logicom/models/Brand.java
+++ src/main/java/fr/logicom/logicom/models/Brand.java
@@ -1,5 +1,30 @@
 package fr.logicom.logicom.models;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Brand {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long brandId;
+	private String brandName;
+	@OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
+	private List<Product> products;
 
 }
