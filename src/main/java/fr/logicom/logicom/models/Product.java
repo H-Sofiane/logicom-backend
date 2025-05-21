fec60e46520757253be310cabacf490b7d73bc96
ssoufiane <souf55@hotmail.fr>
2025-05-21 10:24:52 +0200

    ajou model product

--- src/main/java/fr/logicom/logicom/models/Product.java
+++ src/main/java/fr/logicom/logicom/models/Product.java
@@ -1,5 +1,39 @@
 package fr.logicom.logicom.models;
 
+import java.util.List;
+
+import org.hibernate.annotations.CascadeType;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Product {
+	 @Id
+	 @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nom;
+	private String description;
+	private double prix;
+	private int stock;
+	 @OneToOne
+	private Category category;
+	 @OneToOne
+	private Brand brand;
+	 
+	 @ManyToMany(mappedBy = "products")
+	  private List<Order> orders;
+	
+	
+	
 
 }
